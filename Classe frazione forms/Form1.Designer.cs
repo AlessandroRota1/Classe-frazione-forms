e531d8a644c031937f72fc540c9800e568a8320d
Rota.20073 <rotaale05102006@gmail.com>
2024-01-30 11:34:26 +0100

    Aggiungere i file di progetto.

---
+++ Classe frazione forms/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Classe_frazione_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.D = new System.Windows.Forms.Button();
+            this.Somma = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Ris = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inserisci il numeratore della frazione:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Inserisci il denominatore della frazione:";
+            // 
+            // D
+            // 
+            this.D.Location = new System.Drawing.Point(387, 80);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(134, 38);
+            this.D.TabIndex = 4;
+            this.D.Text = "Semplifica frazione";
+            this.D.UseVisualStyleBackColor = true;
+            this.D.Click += new System.EventHandler(this.D_Click);
+            // 
+            // Somma
+            // 
+            this.Somma.Location = new System.Drawing.Point(387, 134);
+            this.Somma.Name = "Somma";
+            this.Somma.Size = new System.Drawing.Size(134, 38);
+            this.Somma.TabIndex = 5;
+            this.Somma.Text = "Somma";
+            this.Somma.UseVisualStyleBackColor = true;
+            this.Somma.Click += new System.EventHandler(this.Somma_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(387, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 38);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sottrai";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(387, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 38);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Moltiplica";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(387, 294);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 38);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Dividi";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Ris
+            // 
+            this.Ris.Location = new System.Drawing.Point(551, 196);
+            this.Ris.Name = "Ris";
+            this.Ris.Size = new System.Drawing.Size(136, 20);
+            this.Ris.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(548, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Risultato:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Ris);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Somma);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button D;
+        private System.Windows.Forms.Button Somma;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox Ris;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Classe frazione forms/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classe_frazione_forms
+{
+    
+    public partial class Form1 : Form
+    {
+        class frazione
+        {
+            private double numeratore;
+            private double denominatore;
+            public double Numeratore
+            {
+                get => numeratore;
+                set => numeratore = value;
+            }
+            public double Denominatore
+            {
+                get => denominatore;
+                set => denominatore = value;
+            }
+            public string semplificafrazione()
+            {
+                double resto = 0;
+                if (numeratore % denominatore == 0)
+                {
+                    for (double i = numeratore; i < denominatore; i++)
+                    {
+                        if (i % denominatore == 0)
+                        {
+
+                        }
+                    }
+                    //while (denominatore != 0)
+                    //{
+                    //    resto = numeratore % denominatore;
+                    //    numeratore = denominatore;
+                    //    denominatore = resto;
+                    //}
+                    //numeratore = numeratore % resto;
+                    //denominatore = denominatore % resto;
+                }
+                return (numeratore + "/" + denominatore);
+            }
+            public double somma()
+            {
+                return numeratore + denominatore;
+            }
+            public double sottrai()
+            {
+                return numeratore - denominatore;
+            }
+            public double moltiplica()
+            {
+                return numeratore * denominatore;
+            }
+            public double dividi()
+            {
+                return numeratore / denominatore;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void D_Click(object sender, EventArgs e)
+        {
+            frazione semplif = new frazione();
+            Ris.Text = semplif.semplificafrazione();
+        }
+
+        private void Somma_Click(object sender, EventArgs e)
+        {
+            frazione semplif = new frazione();
+            semplif.Numeratore = double.Parse(textBox1.Text);
+            semplif.Denominatore = double.Parse(textBox2.Text);
+            Ris.Text = semplif.somma().ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frazione semplif = new frazione();
+            semplif.Numeratore = double.Parse(textBox1.Text);
+            semplif.Denominatore = double.Parse(textBox2.Text);
+            Ris.Text = semplif.sottrai().ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            frazione semplif = new frazione();
+            semplif.Numeratore = double.Parse(textBox1.Text);
+            semplif.Denominatore = double.Parse(textBox2.Text);
+            Ris.Text = semplif.moltiplica().ToString();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            frazione semplif = new frazione();
+            semplif.Numeratore = double.Parse(textBox1.Text);
+            semplif.Denominatore = double.Parse(textBox2.Text);
+            Ris.Text = semplif.dividi().ToString();
+        }
+    }
+}
